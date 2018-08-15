a3c3e3b154694df0570b3a067ca453f2660244eb
pjbartlein <bartlein@uoregon.edu>
2018-08-14 17:02:55 -0700

    Path changes

--- f90/main_programs/cal_adjust_PMIP3.f90
+++ f90/main_programs/cal_adjust_PMIP3.f90
@@ -117,18 +117,18 @@ max_threads = max_threads - 2 ! to be able to do other things
 call omp_set_num_threads(max_threads)
 
 ! path to netCDF folders and files (i.e. /source/*.nc (input) and /adjusted/*.nc (output)) "
-nc_path = "/Projects/Calendar/data/nc_files/" ! Windows path
-!nc_path = "/Users/bartlein/Projects/Calendar/PaleoCalendarAdjust/data/nc_files/"    ! Mac path
+!nc_path = "/Projects/Calendar/data/nc_files/" ! Windows path
+nc_path = "/Users/bartlein/Projects/Calendar/PaleoCalendarAdjust/data/nc_files/"    ! Mac path
 
 ! debugging output files
-debugpath="/Projects/Calendar/PaleoCalendarAdjust/data/debug_files/" ! Windows path
-!debugpath="/Users/bartlein/Projects/Calendar/PaleoCalendarAdjust/data/debug_files/" ! Mac path
+!debugpath="/Projects/Calendar/PaleoCalendarAdjust/data/debug_files/" ! Windows path
+debugpath="/Users/bartlein/Projects/Calendar/PaleoCalendarAdjust/data/debug_files/" ! Mac path
 debugfile="debug_cal_adjust.dat"
 open (10, file=trim(debugpath)//trim(debugfile))
 
 ! info files
-infopath = "/Projects/Calendar/PaleoCalendarAdjust/data/info_files/" ! Windows path
-!infopath = "/Users/bartlein/Projects/Calendar/PaleoCalendarAdjust/data/info_files/"  ! Mac path
+!infopath = "/Projects/Calendar/PaleoCalendarAdjust/data/info_files/" ! Windows path
+infopath = "/Users/bartlein/Projects/Calendar/PaleoCalendarAdjust/data/info_files/"  ! Mac path
 infofile = "cal_adj_info.csv"
 
 ! open the info file, and loop over specified calendar tables
